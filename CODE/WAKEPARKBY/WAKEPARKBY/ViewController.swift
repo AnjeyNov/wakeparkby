33db585b169fac715bb66f0c936b9c4cffecbd63
Anjey Novicki <anjeynov@gmail.com>
2020-07-07 13:08:10 +0300

    rename project

---
+++ CODE/WAKEPARKBY/WAKEPARKBY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WAKEPRKBY
+//
+//  Created by Anjey Novicki on 7/7/20.
+//  Copyright © 2020 Anjey Novicki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
