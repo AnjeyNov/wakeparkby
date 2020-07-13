165360d079809dbd04dc167691fa9c0589ca4f8f
Anjey Novicki <anjeynov@gmail.com>
2020-07-13 21:35:20 +0300

    feat: add RegistryViewController.swift

---
+++ CODE/WAKEPARKBY/WAKEPARKBY/RegistryViewController/RegistryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegistryViewController.swift
+//  WAKEPARKBY
+//
+//  Created by Anjey Novicki on 7/13/20.
+//  Copyright © 2020 Anjey Novicki. All rights reserved.
+//
+
+import UIKit
+
+class RegistryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tapScreen = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard(sender:)))
+        tapScreen.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tapScreen)
+    }
+    
+    @objc func dismissKeyboard(sender: UITapGestureRecognizer) {
+           self.view.endEditing(true)
+       }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
