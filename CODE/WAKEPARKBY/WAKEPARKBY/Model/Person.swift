7d557dd7845b3be3b74bd1258eb57857f097f79d
Anjey Novicki <anjeynov@gmail.com>
2020-07-13 16:54:35 +0300

    fear: add Person model

---
+++ CODE/WAKEPARKBY/WAKEPARKBY/Model/Person.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  WAKEPARKBY
+//
+//  Created by Anjey Novicki on 7/13/20.
+//  Copyright © 2020 Anjey Novicki. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    var phoneNumber: String = ""
+    var name: String = ""
+    var surname: String = ""
+    var subscription: Int = -1
+}
