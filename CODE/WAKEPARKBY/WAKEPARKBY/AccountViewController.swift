e682678ef9174fa0d889f0d803e46ae37be13d27
Anjey Novicki <anjeynov@gmail.com>
2020-08-22 09:47:06 +0300

    feat: replacement of SubscriptionViewController for AccountViewController

---
+++ CODE/WAKEPARKBY/WAKEPARKBY/AccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccountViewController.swift
+//  WAKEPARKBY
+//
+//  Created by Anjey Novicki on 8/15/20.
+//  Copyright © 2020 Anjey Novicki. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var surnameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var bdayLabel: UILabel!
+    @IBOutlet weak var subscriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = user.name
+        surnameLabel.text = user.surname
+        phoneLabel.text = user.phoneNumber
+        bdayLabel.text = user.bday
+        subscriptionLabel.text = String(user.subscription)
+        
+    }
+    
+}
